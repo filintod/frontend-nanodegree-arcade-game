364d91e1c2b01b94f92d2667dcae9b4264fe4e93
Filinto Duran <filinto_duran@labs.att.com>
2015-02-02 15:03:02 -0600

    minor change of collision detection after rendering

--- js/engine.js
+++ js/engine.js
@@ -87,8 +87,8 @@ var Engine = (function(global) {
             /* Call our update/render functions, pass along the time delta to
              * our update function since it may be used for smooth animation.
              */
-            update(dt);
             renderEntities(fgCtx);
+            update(dt);
 
             /* Set our lastTime variable which is used to determine the time delta
              * for the next time this function is called.
